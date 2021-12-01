746cdace2a2b9dd59e634843d2acc316efb383c5
ahmedwael9 <awamfd99@gmail.com>
2021-12-01 21:33:22 +0200

    add project page

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,86 @@ AOS.init({
     once: true,
     mirror: true
 });
+
+
+// Save EN default language
+if (!document.cookie.indexOf("language=") >= 0) {
+  // set a new cookie
+  expiry = new Date();
+  expiry.setTime(expiry.getTime()+(10*60*36000)); // Ten minutes
+  document.cookie = "language=en; expires=" + expiry.toGMTString();
+}
+
+
+$.getJSON(`assets/lang/${getCookie("language")}.json`, function(data) {
+    setTextLang(data);
+});
+
+
+//change language ON CLICK
+document.querySelector('[data-button="langChange"]').addEventListener('click',function(){
+    let currentLanguage = getCookie("language");
+    // Get time
+    expiry = new Date();
+    expiry.setTime(expiry.getTime()+(10*60*36000)); // Ten minutes
+    if(currentLanguage=='en'){
+        document.cookie = "language=ar; expires=" + expiry.toGMTString();
+    }
+    else {
+        document.cookie = "language=en; expires=" + expiry.toGMTString();
+    }
+    $.getJSON(`assets/lang/${getCookie("language")}.json`, function(data) {
+        setTextLang(data);
+    });
+});
+
+
+function setTextLang(data){
+    document.querySelector('[data-text="navbar-homepage"]').innerHTML=data.navbar_homepage;
+    
+
+}
+
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
+
+// document.getElementById('name');
+// document.getElementsByClassName('name');
+// document.getElementsByTagName('p');
+
+
+// // document.querySelectorAll('p');
+// var p = document.querySelectorAll('p');
+// p.style.color="red"
+// console.log(p);
+
+// function show(){
+//     alert("cfhcfh");
+// }
+
+// document.getElementById("obay").addEventListener('click', function(){
+//     show();
+// })
+
+// function getname() {
+//     p.forEach(element => {
+//         element.innerHTML = "<h1>obay</h1>";
+//     });
+// }
+
--- assets/js/projects.js
+++ assets/js/projects.js
@@ -7,8 +7,20 @@ $.getJSON("data.json", function(data) {
     
         let allCards = "";
         data.myProjects.forEach(project => {
-            allCards += '<div class="col-12 col-sm-6 col-md-4 my-2" data-aos="fade-up" data-aos-delay="200" ><div class="card"><img class="card-img-top" src="'+project.image+'" alt="Card image" /><div class="card-body"><h4 class="text-orange text-uppercase card-title">' + project.title + '</h4><p class="de card-text">'+project.description+'</p><a href="/project.html?id='+project.id+'" class="btn btn-danger">See Profile</a></div></div></div>';
+            allCards += `
+            <div class="col-12 col-sm-6 col-md-4 my-2" data-aos="fade-up" data-aos-delay="200" >
+                <div class="card">
+                    <div class="mockup"style="background-image: url(${project.image});"></div>
+                    <div class="card-body">
+                        <h4 class="text-orange text-uppercase card-title">${ project.title} </h4>
+                        <p class="description card-text">${project.description}</p>
+                        <a href="/project.html?id=${project.id}" class="btn btn-danger">See Profile</a>
+                    </div>
+                </div>
+            </div>`;
         });
+
+
         container.innerHTML = allCards;
     }
 
@@ -16,7 +28,26 @@ $.getJSON("data.json", function(data) {
     var url_string = window.location.href; 
     var url = new URL(url_string);
     var id = url.searchParams.get("id");
+    id--;
     console.log(id);
 
-    document.querySelector('[data-text="title"]').innerHTML= data.myProjects[id-1].title;
+     document.querySelector('[data-text="projectTitle"]').innerHTML= data.myProjects[id].title;
+     document.querySelector('[data-text="projectDescription"]').innerHTML= data.myProjects[id].description;
+
+
+
+    const containerMedia=document.querySelector('[data-container="media"]');
+    let imges="";
+    imges+=`<div data-aos="fade-up"><img src="${data.myProjects[id].image}" class="w-100 py-2" alt=""></div>`;
+
+    
+    data.myProjects[id].media.forEach(element => {
+        imges+=`<img src="${element}" class="w-100 py-2" alt="">`;
+        
+    });
+     
+
+
+
+     containerMedia.innerHTML= imges
 });
