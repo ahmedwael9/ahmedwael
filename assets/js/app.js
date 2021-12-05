61b048a1a0d2ddc5c512e4a36af56c326a9a8b69
ahmedwael9 <awamfd99@gmail.com>
2021-12-05 20:03:31 +0200

    file update

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,26 +48,69 @@ function setTextLang(data){
     document.querySelector('body').setAttribute("dir", "rtl")
     document.querySelector('#bootstrap').setAttribute("href", "https://cdn.rtlcss.com/bootstrap/v4.5.3/css/bootstrap.min.css");
   }
+  document.querySelector('[data-text="navbar-logo"]').innerHTML=data.navbar_logo;
   document.querySelector('[data-text="navbar-homepage"]').innerHTML=data.navbar_homepage;
   document.querySelector('[data-text="navbar-projects"]').innerHTML=data.navbar_projects;
   document.querySelector('[data-text="navbar-language"]').innerHTML=data.navbar_language;
-  document.querySelector('[data-text="intro-name"]').innerHTML=data.inrto_name;
-  document.querySelector('[data-text="intro-myname"]').innerHTML=data.inrto_myname;
-  document.querySelector('[data-text="intro-jop"]').innerHTML=data.intro_jop;
-  document.querySelector('[data-text="intro-description"]').innerHTML=data.intro_description;
-  document.querySelector('[data-text="intro-description"]').innerHTML=data.intro_description;
-  document.querySelector('[data-text="intro-buttonWork"]').innerHTML=data.intro_buttonWork;
-  document.querySelector('[data-text="intro-buttonresume"]').innerHTML=data.intro_buttonresume;
-  document.querySelector('[data-text="navbar-logo"]').innerHTML=data.navbar_logo;
-  document.querySelector('[data-text="intro-ido"]').innerHTML=data.intro_ido;
-  document.querySelector('[data-text="intro-idoDescription"]').innerHTML=data.intro_idoDescription;
-  document.querySelector('[data-text="card-titleapp"]').innerHTML=data.card_titleapp;
-  document.querySelector('[data-text="card-descriptionapp"]').innerHTML=data.card_descriptionapp;
-  document.querySelector('[data-text="card-titlemobile"]').innerHTML=data.card_titlemobile;
-  document.querySelector('[data-text="card-descriptionmobile"]').innerHTML=data.card_descriptionmobile;
-  document.querySelector('[data-text="card-titleweb"]').innerHTML=data.card_titleweb;
-  document.querySelector('[data-text="card-descriptionweb"]').innerHTML=data.card_descriptionweb;
+  // If homepage
+  if(document.querySelector('[data-text="intro-name"]')) {
+    document.querySelector('[data-text="intro-name"]').innerHTML=data.inrto_name;
+    document.querySelector('[data-text="intro-myname"]').innerHTML=data.inrto_myname;
+    document.querySelector('[data-text="intro-jop"]').innerHTML=data.intro_jop;
+    document.querySelector('[data-text="intro-description"]').innerHTML=data.intro_description;
+    document.querySelector('[data-text="intro-description"]').innerHTML=data.intro_description;
+    document.querySelector('[data-text="intro-buttonWork"]').innerHTML=data.intro_buttonWork;
+    document.querySelector('[data-text="intro-buttonresume"]').innerHTML=data.intro_buttonresume;
+    document.querySelector('[data-text="intro-ido"]').innerHTML=data.intro_ido;
+    document.querySelector('[data-text="intro-idoDescription"]').innerHTML=data.intro_idoDescription;
+    document.querySelector('[data-text="card-titleapp"]').innerHTML=data.card_titleapp;
+    document.querySelector('[data-text="card-descriptionapp"]').innerHTML=data.card_descriptionapp;
+    document.querySelector('[data-text="card-titlemobile"]').innerHTML=data.card_titlemobile;
+    document.querySelector('[data-text="card-descriptionmobile"]').innerHTML=data.card_descriptionmobile;
+    document.querySelector('[data-text="card-titleweb"]').innerHTML=data.card_titleweb;
+    document.querySelector('[data-text="card-descriptionweb"]').innerHTML=data.card_descriptionweb;
+  }
+   // Show projects in homepage
+    if(document.querySelector('[data-container="projects"]')){
+        const container = document.querySelector('[data-container="projects"]');
+    
+        let allCards = "";
+        data.myProjects.forEach(project => {
+            allCards += `
+            <div class="col-12 col-sm-6 col-md-4 my-2" data-aos="fade-up" data-aos-delay="200" >
+                <div class="card">
+                    <div class="mockup"style="background-image: url(${project.image});"></div>
+                    <div class="cardbody card-body">
+                    <div>
+                    <h4 class="text-orange text-uppercase card-title">${ project.title} </h4>
+                    <p class="description card-text">${project.description}</p>
+                    
+                    </div>
+                        <a href="/project.html?id=${project.id}" class="seeprofile btn btn-danger mt-2">See Profile</a>
+                    </div>
+                </div>
+            </div>`;
+        });
+        container.innerHTML = allCards;
+    }
+
+    // Show project data
+    var url_string = window.location.href; 
+    var url = new URL(url_string);
+    var id = url.searchParams.get("id");
+    id--;
+
+     document.querySelector('[data-text="projectTitle"]').innerHTML= data.myProjects[id].title;
+     document.querySelector('[data-text="projectDescription"]').innerHTML= data.myProjects[id].description;
+    const containerMedia=document.querySelector('[data-container="media"]');
+    let imges="";
+    imges+=`<div data-aos="fade-up"><img src="${data.myProjects[id].image}" class="w-100 py-2" alt=""></div>`;
+    data.myProjects[id].media.forEach(element => {
+        imges+=`<img src="${element}" class="w-100 py-2" alt="">`;
+    });
+     containerMedia.innerHTML= imges
 }
+
 window.onload = function(){
   $('.owl-carousel').owlCarousel({
       loop:true,
